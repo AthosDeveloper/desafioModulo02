98d7a4aac8635c5ec10d453760c7bb292bd72f7f
AthosDeveloper <athos.silva@zup.com.br>
2023-06-06 09:53:45 -0300

    upidating file

--- NumeroAleatorio.java
+++ NumeroAleatorio.java
@@ -13,7 +13,9 @@ public class NumeroAleatorio {
           int nivel = 0;
           String resposta = "nao";
           int numero = 0;
-
+int vezes = 0;
+int vitoria = 0;
+int derrota = 0;
           do {
               System.out.println("Defina o nível desejado: 1 para o mais fácil e 3 para o mais difícil: ");
               nivel = sc.nextInt();
@@ -25,9 +27,11 @@ public class NumeroAleatorio {
        if (numero == varia) {
                       System.out.println("Você escolheu o número: " + numero + " e o número gerado foi: " + varia);
                       System.out.println("Você acertou!");
+                      vitoria ++;
                       pontos += 10;
                   } else {
                       System.out.println("Você perdeu! O número sorteado era: " + varia);
+                      derrota ++;
                   }
               } else if (nivel == 2) {
                   System.out.println("Escolha um número de 0 a 4.");
@@ -37,11 +41,13 @@ public class NumeroAleatorio {
                       System.out.println("Você escolheu o número: " + numero + " e o número gerado foi: " + varia);
                       System.out.println("Você acertou!");
 pontos += 10;
+vitoria ++;
                   } else if (numero == varia - 1 || numero == varia + 1) {
          System.out.println("Seu número escolhido está próximo do número sorteado.");
                       pontos += 5;
                   } else {
                       System.out.println("Você perdeu! O número sorteado era: " + varia);
+                      derrota ++;
                   }
               } else if (nivel == 3) {
                   System.out.println("Escolha um número de 0 a 9.");
@@ -52,22 +58,24 @@ pontos += 10;
                       System.out.println("Você escolheu o número: " + numero + " e o número gerado foi: " + varia);
                       System.out.println("Você acertou!");
                       pontos += 10;
+                      vitoria ++;
                   } else if (numero == varia - 1 || numero == varia + 1) {
                       System.out.println("Seu número escolhido está próximo do número sorteado.");
                       pontos += 5;
                   } else {
                       System.out.println("Você perdeu! O número sorteado era: " + varia);
+                      derrota ++;
                   }
               } else {
                   System.out.println("Nível inválido!");
                   continue;
               }
-
+vezes ++;
               System.out.println("Você deseja jogar novamente? (Sim ou Não)");
               resposta = sc.next().toLowerCase();
           } while (!resposta.equals("nao"));
 
-          System.out.println("Pontuação final: " + pontos);
+          System.out.println("Pontuação final: " + pontos + " você jogou: " + vezes + " vezes " + " vitórias: " + vitoria + " erros: " + derrota);
       }
 }
 
